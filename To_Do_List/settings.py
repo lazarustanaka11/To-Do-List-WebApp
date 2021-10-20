33e1480abb89595a0e8e8081db666338fc56afba
Lazarus Tanaka Mtake <36960181+lazarustanaka11@users.noreply.github.com>
2021-10-20 15:47:16 +0200

    Update settings.py

--- To_Do_List/settings.py
+++ To_Do_List/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8(%x)2%c(*-k3oeow(u5y6)8&22!6x=i&q7*z!7v$jzz+kv-%3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ 'lazarus-to-do-list.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = [ 'https://lazarus-to-do-list.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -121,7 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 LOGIN_URL= 'sign-in'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'templates')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles
+                           
+                           ')
 
 STATIC_URL = '/static/'
 
